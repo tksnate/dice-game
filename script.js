2e572c4c26ff9a259dba66bdae388df4590fc743
tksnate <tksnate@gmail.com>
2023-04-16 15:33:21 -0700

    active player js update

--- script.js
+++ script.js
@@ -2,17 +2,20 @@
 
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
+const current0 = document.querySelector('#current--0');
+const current1 = document.querySelector('#current--1');
 const dice = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn-new');
-const btnRoll = document.querySelector('.btn-roll');
-const btnHold = document.querySelector('.btn-hold');
-// const current0 = document.querySelector('.current--0');
-// const current1= document.querySelector('.current--1');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
 
+const scores = [0, 0];
 score0.textContent = 0;
 score1.textContent = 0;
-let current0 = 0;
-let current1 = 0;
+let currentScore = 0;
+let activePlayer = 0;
 
 dice.classList.add('hidden');
 
@@ -21,10 +24,16 @@ btnRoll.addEventListener('click', function () {
   dice.src = `dice-${roll}.png`;
   dice.classList.remove('hidden');
 
-  if (roll === 1) {
+  if (roll !== 1) {
+    currentScore += roll;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
     console.log('you rolled a 1. switch player');
-  } else if (roll > 1) {
-    current0 = current0 + roll;
-    console.log(current0);
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
   }
 });
